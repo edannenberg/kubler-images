03b9c9de2e05a9459b76cbfe521e5a4280262bc1
Erik Dannenberg <erik.dannenberg@bbe-consulting.de>
2019-01-25 22:47:34 +0100

    [graph-easy] Initial commit

---
+++ images/graph-easy/README.md
@@ -0,0 +1,16 @@
+## kubler/graph-easy
+
+Run this [graph-easy][] image with:
+
+    $ docker run -it --rm kubler/graph-easy graph-easy --help
+
+The image also includes graphviz if you prefer to use it directly:
+
+    $ docker run -it --rm kubler/graph-easy dot --help
+
+---
+
+[Last Build][packages]
+
+[graph-easy]: http://bloodgate.com/perl/graph/
+[packages]: PACKAGES.md
---
+++ images/graph-easy/build.sh
@@ -0,0 +1,28 @@
+#
+# Kubler phase 1 config, pick installed packages and/or customize the build
+#
+
+_packages="dev-perl/Graph-Easy"
+
+#
+# This hook is called just before starting the build of the root fs
+#
+configure_rootfs_build()
+{
+    update_use -introspection
+    update_use 'media-gfx/graphviz' -nls
+    update_use 'media-libs/harfbuzz' -graphite
+    update_use 'media-libs/gd' +fontconfig +jpeg +truetype +png
+    # graphviz ebuild calls 'dot -c || die' as part of postinstall. Fake dot and run the setup via Dockerfile instead.
+    ln -s /bin/true /usr/bin/dot
+    # no python please.
+    provide_package app-eselect/eselect-fontconfig dev-lang/python-exec app-eselect/eselect-python dev-lang/python
+}
+
+#
+# This hook is called just before packaging the root fs tar ball, ideal for any post-install tasks, clean up, etc
+#
+finish_rootfs_build()
+{
+    rm /usr/bin/dot
+}
