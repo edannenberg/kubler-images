519c74928021a9f5060d5295f0853cce2d48bc63
Erik Dannenberg <erik.dannenberg@bbe-consulting.de>
2019-10-03 13:27:06 +0200

    [kibana] Fix build via binary package workaround

--- images/kibana/build.sh
+++ images/kibana/build.sh
@@ -5,12 +5,19 @@ _packages="app-admin/su-exec www-apps/kibana-bin"
 # skip all dependencies of kibana
 _emerge_opt="--nodeps"
 
+configure_builder()
+{
+    update_keywords 'www-apps/kibana-bin' '+~amd64'
+    # install fails with custom ROOT, create binary package first via normal install
+    emerge www-apps/kibana-bin
+}
+
 #
 # This hook is called just before starting the build of the root fs
 #
 configure_rootfs_build()
 {
-    update_keywords 'www-apps/kibana-bin' '+~amd64'
+    :
 }
 
 #
