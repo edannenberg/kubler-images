c1639ebeb83e591fdd463fa238625763c90be977
Erik Dannenberg <erik.dannenberg@bbe-consulting.de>
2019-10-16 15:03:20 +0200

    [bash] Fix grep --colour check

--- images/bash/build.sh
+++ images/bash/build.sh
@@ -19,9 +19,8 @@ configure_rootfs_build()
 #
 finish_rootfs_build()
 {
-    cp /etc/{passwd,group} "${_EMERGE_ROOT}"/etc
-    cp /etc/inputrc "${_EMERGE_ROOT}"/etc
-    # busybox's grep doesn't support --color=auto args, add a check for the alias in bashrc
-    sed-or-die 'alias grep=' 'grep --color=auto root /etc/group \&> /dev/null \&\& alias grep=' \
+    cp /etc/{passwd,group,inputrc} "${_EMERGE_ROOT}"/etc
+    # busybox's grep doesn't support --colour=auto args, add a check for the alias in bashrc
+    sed-or-die 'alias grep=' 'grep --colour=auto root /etc/group \&> /dev/null \&\& alias grep=' \
         "${_EMERGE_ROOT}"/etc/bash/bashrc
 }
