f20c839e1632b137c9bffcab771235804013706a
Erik Dannenberg <erik.dannenberg@bbe-consulting.de>
2019-11-30 17:23:18 +0100

    [grafana] Fix build

--- images/grafana/build.sh
+++ images/grafana/build.sh
@@ -1,17 +1,28 @@
 #
 # Kubler phase 1 config, pick installed packages and/or customize the build
 #
-_packages="www-apps/grafana"
+_packages="www-apps/grafana net-libs/nodejs sys-apps/yarn"
+
+configure_builder()
+{
+    # grafana apparently already on go 1.13.x which has some bug fixes missing missing in prev. versions
+    update_keywords '=dev-lang/go-1.13.4' '+~amd64'
+    emerge -u dev-lang/go
+    # ..still running on old nodejs 10.x though :/
+    mask_package '>=net-libs/nodejs-12.13.0'
+    update_keywords 'sys-apps/yarn' '+~amd64'
+    emerge -u net-libs/nodejs
+}
 
 #
 # This hook is called just before starting the build of the root fs
 #
 configure_rootfs_build()
 {
-    # for some unknown reason the "go get" cmd now returns with exit signal 1 and fails the build
-    # very dirty workaround for now, gonna investigate later. why are go builds so freaking brittle all the time :/
-    cp /var/sync/portage/eclass/golang-vcs.eclass ~/golang-vcs.eclass
-    sed -i 's/"$@" || die "failed $@"/"$@"/g' /var/sync/portage/eclass/golang-vcs.eclass
+    # add user/group for unprivileged container usage
+    groupadd -g 700 nodejs
+    useradd -u 7000 -g nodejs -d /home/nodejs nodejs
+    mkdir -p "${_EMERGE_ROOT}"/home/nodejs
 }
 
 #
@@ -19,9 +30,7 @@ configure_rootfs_build()
 #
 finish_rootfs_build()
 {
-
     mkdir -p "${_EMERGE_ROOT}"/opt/grafana
     ln -sr "${_EMERGE_ROOT}"/usr/share/grafana/{conf,public} "${_EMERGE_ROOT}"/opt/grafana/
-    # revert eclass hack
-    mv ~/golang-vcs.eclass /var/sync/portage/eclass/golang-vcs.eclass
+    copy_gcc_libs
 }
